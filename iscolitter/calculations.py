283fa97b88d8384789899351203d1f5b6fe570e5
John Sorensen <john.sorensen@usda.gov>
2025-08-08 16:24:38 -0500

    created dataframe of field constants

---
+++ iscolitter/calculations.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+field_constants = {
+                    "field" : ["SW12", "SW17", "W1", 
+                              "W6", "W10", "W12",
+                              "W13", "Y2", "Y6", 
+                              "Y8", "Y10", "Y13", 
+                              "Y14"],
+                    "area (ac)" : [2.97, 2.99, 174.00,
+                                  42.30, 19.80, 9.90,
+                                  11.30, 132.00, 16.30,
+                                  20.80, 18.50, 11.40,
+                                  5.60],
+                     "landuse" : ["pasture", "pasture", "mixed",
+                                  "mixed", "pasture", "cultivated",
+                                  "cultivated", "mixed", "cultivated",
+                                  "cultivated", "cultivated", "cultivated",
+                                  "pasture"],
+                    "1-bottle composite flow interval (mm)" : [1.32, 1.32, 1.32,
+                                                               1.32, 1.32, 1.32,
+                                                               1.32, 1.32, 1.32,
+                                                               1.32, 1.32, 1.32,
+                                                               1.32],
+                    "sampling interval (ft3)" : [565.9, 565.9, 32890.2,
+                                                 8000, 3735, 1864,
+                                                 2132, 24868.2, 3076,
+                                                 3915, 3496, 2132,
+                                                 1056]
+                     }
+
+field_df = pd.DataFrame(field_constants)
+field_df["sampling interval (mgal)"] = field_df["sampling interval (ft3)"] * (1/133680.556)
+print(field_df)
