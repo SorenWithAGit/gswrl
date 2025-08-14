ab5353196ebbddf63e7ba16369b262092793378f
John Sorensen <john.sorensen@usda.gov>
2025-08-14 09:46:21 -0500

    moved functions to module lab_data

--- iscolitter/isco.py
+++ iscolitter/isco.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import read_sampler as rs
 import calculations as cal
+import read_lab_data as rld
 import glob
 
 pd.options.mode.copy_on_write = True
@@ -16,100 +17,9 @@ for file in sampler_files:
     sampler_data = pd.concat([sampler_data, sampler_df]).reset_index(drop = True)
 # print(sampler_data)
 
-lab_data = pd.read_excel(r"I:\USDA-ARS\Doug Smith\Riesel\Water Quaility\raw_data\2019\wq_nutrients_2019.xlsx")
-lab_data["Sample Date"] = lab_data["Sample Date"].dt.strftime("%m-%d-%Y")
+lab_data_path = r"I:\USDA-ARS\Doug Smith\Riesel\Water Quaility\raw_data\2019\wq_nutrients_2019.xlsx"
 
-nutrient_data = lab_data[lab_data["Sample Type"] == "N"]
-rep_lst = []
-for ind in nutrient_data.index:
-    rep = nutrient_data["Sample Name"][ind].split(" ")[1]
-    rep_lst.append(rep)
-nutrient_data["Replicate"] = rep_lst
-
-nutrient_acid_data = lab_data[lab_data["Sample Type"] == "NutAcid"]
-na_rep_lst = []
-for index in nutrient_acid_data.index:
-    na_rep = nutrient_acid_data["Sample Name"][index].split(" ")[1]
-    na_rep_lst.append(na_rep)
-nutrient_acid_data["Replicate"] = na_rep_lst
-
-no3_lst_1 = []
-nh3_lst_1 = []
-po4_lst_1 = []
-
-for i in sampler_data.index:
-    for iter in nutrient_data.index:
-        if sampler_data["Site"][i] == nutrient_data["Sample Name"][iter].split(" ")[0] \
-            and sampler_data["Date"][i] == nutrient_data["Sample Date"][iter] and nutrient_data["Replicate"][iter] == "#1":
-            # print(str(sampler_data["Site"][i]) + " Found! " + str(nutrient_data["Sample Name"][iter]) + " " + \
-            #       str(nutrient_data["Sample Date"][iter]))
-            no3_lst_1.append(nutrient_data["Nitrate Nitrite- Results[mg N/liter]"][iter])
-            nh3_lst_1.append(nutrient_data["Ammonia- Results[mg N/liter]"][iter])
-            po4_lst_1.append(nutrient_data["Phosphate- Results[mg P/liter]"][iter])
-
-no3_lst_2 = []
-nh3_lst_2 = []
-po4_lst_2 = []
-
-for i in sampler_data.index:
-    for iter in nutrient_data.index:
-        if sampler_data["Site"][i] == nutrient_data["Sample Name"][iter].split(" ")[0] \
-            and sampler_data["Date"][i] == nutrient_data["Sample Date"][iter] and nutrient_data["Replicate"][iter] == "#2":
-            # print(str(sampler_data["Site"][i]) + " Found! " + str(nutrient_data["Sample Name"][iter]) + " " + \
-            #       str(nutrient_data["Sample Date"][iter]))
-            no3_lst_2.append(nutrient_data["Nitrate Nitrite- Results[mg N/liter]"][iter])
-            nh3_lst_2.append(nutrient_data["Ammonia- Results[mg N/liter]"][iter])
-            po4_lst_2.append(nutrient_data["Phosphate- Results[mg P/liter]"][iter])
-
-storm_df = sampler_data
-storm_df["NO3-N [mg N/liter] smpl 1"] = no3_lst_1
-storm_df["NH3-N [mg N/liter] smpl 1"] = nh3_lst_1
-storm_df["PO4-P [mg P/liter] smpl 1"] = po4_lst_1
-storm_df["NO3-N [mg N/liter] smpl 2"] = no3_lst_2
-storm_df["NH3-N [mg N/liter] smpl 2"] = nh3_lst_2
-storm_df["PO4-P [mg P/liter] smpl 2"] = po4_lst_2
-storm_df["NO3-N [mg N/liter] avg"] = (storm_df["NO3-N [mg N/liter] smpl 1"] + storm_df["NO3-N [mg N/liter] smpl 2"]) / 2
-storm_df["NH3-N [mg N/liter] avg"] = (storm_df["NH3-N [mg N/liter] smpl 1"] + storm_df["NH3-N [mg N/liter] smpl 2"]) / 2
-storm_df["PO4-P [mg P/liter] avg"] = (storm_df["PO4-P [mg P/liter] smpl 1"] + storm_df["PO4-P [mg P/liter] smpl 2"]) / 2
-
-acid_no3_lst_1 = []
-acid_nh3_lst_1 = []
-acid_po4_lst_1 = []
-
-for id in sampler_data.index:
-    for count in nutrient_acid_data.index:
-        if sampler_data["Site"][id] == nutrient_acid_data["Sample Name"][count].split(" ")[0] \
-            and sampler_data["Date"][id] == nutrient_acid_data["Sample Date"][count] and nutrient_acid_data["Replicate"][count] == "#1":
-            # print(str(sampler_data["Site"][id]) + " Found! " + str(nutrient_acid_data["Sample Name"][count]) + " " + \
-            #       str(nutrient_acid_data["Sample Date"][count]))
-            acid_no3_lst_1.append(nutrient_acid_data["Nitrate Nitrite- Results[mg N/liter]"][count])
-            acid_nh3_lst_1.append(nutrient_acid_data["Ammonia- Results[mg N/liter]"][count])
-            acid_po4_lst_1.append(nutrient_acid_data["Phosphate- Results[mg P/liter]"][count])
-
-acid_no3_lst_2 = []
-acid_nh3_lst_2 = []
-acid_po4_lst_2 = []
-
-for id in sampler_data.index:
-    for count in nutrient_acid_data.index:
-        if sampler_data["Site"][id] == nutrient_acid_data["Sample Name"][count].split(" ")[0] \
-            and sampler_data["Date"][id] == nutrient_acid_data["Sample Date"][count] and nutrient_acid_data["Replicate"][count] == "#2":
-            # print(str(sampler_data["Site"][id]) + " Found! " + str(nutrient_data["Sample Name"][count]) + " " + \
-            #       str(nutrient_data["Sample Date"][count]))
-            acid_no3_lst_2.append(nutrient_acid_data["Nitrate Nitrite- Results[mg N/liter]"][count])
-            acid_nh3_lst_2.append(nutrient_acid_data["Ammonia- Results[mg N/liter]"][count])
-            acid_po4_lst_2.append(nutrient_acid_data["Phosphate- Results[mg P/liter]"][count])
-
-acid_storm_df = sampler_data
-acid_storm_df["NO3-N [mg N/liter] smpl 1"] = acid_no3_lst_1
-acid_storm_df["NH3-N [mg N/liter] smpl 1"] = acid_nh3_lst_1
-acid_storm_df["PO4-P [mg P/liter] smpl 1"] = acid_po4_lst_1
-acid_storm_df["NO3-N [mg N/liter] smpl 2"] = acid_no3_lst_2
-acid_storm_df["NH3-N [mg N/liter] smpl 2"] = acid_nh3_lst_2
-acid_storm_df["PO4-P [mg P/liter] smpl 2"] = acid_po4_lst_2
-acid_storm_df["NO3-N [mg N/liter] avg"] = (acid_storm_df["NO3-N [mg N/liter] smpl 1"] + acid_storm_df["NO3-N [mg N/liter] smpl 2"]) / 2
-acid_storm_df["NH3-N [mg N/liter] avg"] = (acid_storm_df["NH3-N [mg N/liter] smpl 1"] + acid_storm_df["NH3-N [mg N/liter] smpl 2"]) / 2
-acid_storm_df["PO4-P [mg P/liter] avg"] = (acid_storm_df["PO4-P [mg P/liter] smpl 1"] + acid_storm_df["PO4-P [mg P/liter] smpl 2"]) / 2
+storm_df = rld.storm_data(lab_data_path, sampler_data)[0]
 
 c = cal.conversions()
 v_ft3 = c.total_vol_ft3(storm_df)
